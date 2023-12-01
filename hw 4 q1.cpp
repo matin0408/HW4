59e30ccccd87193b7ce4ec23610e118dbe4c9e52
matin0408 <150231931+matin0408@users.noreply.github.com>
2023-12-02 00:13:05 +0330

    Add files via upload

---
+++ hw 4 q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+	
+float	m , n , x , y , h ;
+float  z;
+cout <<"please enter number  for m:";
+cin>> m;
+cout <<"please enter number  for n:";
+cin>> n;
+x=m+n;
+y=m-n;
+h=m*n;
+z=(float) m/n;
+cout<<"x:"<< x << "   " <<"y: "<< y << "   " << "z: " << z  << "   "<< "h:" << h;
+
+	
+	
+	
+}
